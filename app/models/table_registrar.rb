027c1c33f45f65839d533a35e9782d7057116ba1
gfiorav <guido.fioravantti@gmail.com>
2017-07-11 13:11:56 +0200

    leave table_registrar alone in pos of new table register

--- app/models/table_registrar.rb
+++ app/models/table_registrar.rb
@@ -8,7 +8,12 @@ module CartoDB
     end
 
     def register(table_name, data_import_id)
-      @table = build_table(table_name, data_import_id)
+      self.table                    = table_klass.new
+      table.user_id                 = user.id
+      # INFO: we're not creating but registering an existent table, so we want fixed, known name
+      table.instance_eval { self[:name] = table_name }
+      table.migrate_existing_table  = table_name
+      table.data_import_id  = data_import_id
       set_metadata_from_data_import_id(table, data_import_id)
       table.save
       table.optimize
@@ -26,26 +31,17 @@ module CartoDB
     attr_reader :table_klass
     attr_writer :table
 
-    def build_table(table_name, data_import_id)
-      table = @table_klass.new
-      table.user_id = @user.id
-      table.instance_eval { self[:name] = table_name }
-      table.migrate_existing_table = table_name
-      table.data_import_id = data_import_id
-      table
-    end
-
-    def external_visualization(data_import_id)
-      external_data_import = Carto::ExternalDataImport.find_by_data_import_id(data_import_id)
-      external_source = Carto::ExternalSource.find(external_data_import.external_source_id)
-      external_source.visualization
-    rescue ActiveRecord::RecordNotFound => _e
-    end
-
     def set_metadata_from_data_import_id(table, data_import_id)
-      if external_visualization(data_import_id)
-        table.description = visualization.description
-        table.set_tag_array(visualization.tags)
+      external_data_import = ExternalDataImport.where(data_import_id: data_import_id).first
+      if external_data_import
+        external_source = CartoDB::Visualization::ExternalSource.where(id: external_data_import.external_source_id).first
+        if external_source
+          visualization = external_source.visualization
+          if visualization
+            table.description = visualization.description
+            table.set_tag_array(visualization.tags)
+          end
+        end
       end
     rescue => e
       CartoDB.notify_exception(e)
