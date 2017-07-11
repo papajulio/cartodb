47b5f9e61d746a8df1e9827e2b4e7d2178008acb
gfiorav <guido.fioravantti@gmail.com>
2017-07-11 12:39:44 +0200

    Refactor table building to build_table method for clarity of register methdo

--- app/models/table_registrar.rb
+++ app/models/table_registrar.rb
@@ -8,12 +8,7 @@ module CartoDB
     end
 
     def register(table_name, data_import_id)
-      self.table                    = table_klass.new
-      table.user_id                 = user.id
-      # INFO: we're not creating but registering an existent table, so we want fixed, known name
-      table.instance_eval { self[:name] = table_name }
-      table.migrate_existing_table  = table_name
-      table.data_import_id  = data_import_id
+      @table = build_table(table_name, data_import_id)
       set_metadata_from_data_import_id(table, data_import_id)
       table.save
       table.optimize
@@ -30,6 +25,15 @@ module CartoDB
 
     attr_reader :table_klass
     attr_writer :table
+
+    def build_table(table_name, data_import_id)
+      table = @table_klass.new
+      table.user_id = @user.id
+      table.instance_eval { self[:name] table_name }
+      table.migrate_existing_table = table_name
+      table.data_import_id = data_import_id
+      table
+    end
 
     def set_metadata_from_data_import_id(table, data_import_id)
       external_data_import = ExternalDataImport.where(data_import_id: data_import_id).first
